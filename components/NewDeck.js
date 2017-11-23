8e7fd5e5e6546825626892d1916f5ce038bb5600
xiaoju <me@xiaoju.io>
2017-11-23 10:29:18 +0100

    feat: newDeck title input field

--- components/NewDeck.js
+++ components/NewDeck.js
@@ -1,17 +1,34 @@
 import React, { Component } from 'react'
-import { StyleSheet, View, Text, Button } from 'react-native'
+import { StyleSheet, View, Text, Button, TextInput } from 'react-native'
 import { secondaryColor, secondaryTextColor} from '../utils/colors'
 import { saveDeckTitle, getDecks } from '../utils/api'
 
 export default class NewDeck extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {inputText: ''};
+  }
+
 	render(){
 		return (
 			<View>
-        <Text style={[styles.bigblue]}>Title for new deck:</Text>
+        <Text style={[styles.bigblue]}>Title:</Text>
+
+        <TextInput
+          style = {{height: 50}}
+          placeholder = 'Type new deck title here'
+          value = {this.state.inputText}
+          multiline = {true}
+          onChangeText = {(inputText) => this.setState({inputText})}
+        />
+
         <Button
           onPress={ () =>
-            saveDeckTitle('dummyDeckTitle')
+            {
+              saveDeckTitle(this.state.inputText);
+              this.setState({inputText: ''});
+            }
           }
           title='submit'
         />
