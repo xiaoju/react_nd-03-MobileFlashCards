735fcfa499d9064673cb0780ecb32f09051b1906
xiaoju <me@xiaoju.io>
2017-12-08 14:14:19 +0100

    feat: re-style Quiz.js

--- components/Quiz.js
+++ components/Quiz.js
@@ -1,28 +1,7 @@
 import React, { Component } from 'react'
-import { View, Text, Button } from 'react-native'
-import { MyStyles } from '../utils/MyStyles'
-import styled from 'styled-components/native'
+import { View, Text, Button, TouchableHighlight } from 'react-native'
 import { FontAwesome } from '@expo/vector-icons'
-import {
-	primaryColor,
-	primaryLightColor,
-	primaryDarkColor,
-	secondaryColor,
-	secondaryLightColor,
-	secondaryDarkColor,
-	primaryTextColor,
-	secondaryTextColor,
-} from '../utils/colors'
-
-const SiconContainer = styled.TouchableHighlight`
-	background-color: ${secondaryDarkColor};
-	margin: 3px;
-	padding: 5px;
-	border-radius: 8px;
-	width: 50px;
-	height: 50px;
-	margin-right: 20px;
-`
+import { MyStyles } from '../utils/MyStyles'
 
 export default class Quiz extends Component {
 
@@ -36,18 +15,14 @@ export default class Quiz extends Component {
   }
 
 	render(){
-
 		return (
-      <View
-        style={[MyStyles.background]}
-        >
-        {
-          this.state.questionNo > this.state.thisDeck.questions.length ?
-          <View>
+      <View style={[MyStyles.background]}>
+        { this.state.questionNo > this.state.thisDeck.questions.length ?
+          <View style={[MyStyles.infoView]}>
             {this.state.thisDeck.questions.length === 0
-              ? <Text style={[MyStyles.text]}>'You need to add cards before you can start the quiz!'</Text>
+              ? <Text style={[MyStyles.text]}>Add cards before starting the quiz!</Text>
               :
-                <View>
+                <View style={[MyStyles.infoView]} >
                   <Text style={[MyStyles.text]}>Quiz is complete!</Text>
                   <Text style={[MyStyles.text]}>Your score:</Text>
                   <Text style={[MyStyles.text]}>
@@ -63,19 +38,17 @@ export default class Quiz extends Component {
           </View>
         }
 
-        <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-around'}}>
-
-          <SiconContainer>
+        <View style={[MyStyles.iconsBar]}>
+          <TouchableHighlight style={[MyStyles.iconContainer]}>
             <FontAwesome
               name='home'
               onPress={() => this.props.navigation.navigate('DeckView',{thisDeck: this.state.thisDeck})}
-              color={secondaryTextColor}
               size={40}
               accessibilityLabel='Return to individual deck view'
             />
-          </SiconContainer>
+          </TouchableHighlight>
 
-          <SiconContainer>
+          <TouchableHighlight style={[MyStyles.iconContainer]}>
             <FontAwesome
               name='refresh'
               onPress={() => this.props.navigation.navigate(
@@ -86,14 +59,13 @@ export default class Quiz extends Component {
                   correctCount: 0,
                 }
               )}
-              color={secondaryTextColor}
               size={40}
               accessibilityLabel='Restart quiz'
             />
-          </SiconContainer>
+          </TouchableHighlight>
 
           { this.state.questionNo > this.state.thisDeck.questions.length ||
-            <SiconContainer>
+            <TouchableHighlight style={[MyStyles.iconContainer]}>
               <FontAwesome
                 name='arrow-right'
                 onPress={() => this.props.navigation.navigate(
@@ -104,11 +76,10 @@ export default class Quiz extends Component {
                     correctCount: this.state.correctCount,
                   }
                 )}
-                color={secondaryTextColor}
                 size={40}
                 accessibilityLabel='Next: view answer'
               />
-            </SiconContainer>
+            </TouchableHighlight>
           }
   			</View>
       </View>
